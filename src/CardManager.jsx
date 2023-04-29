d8210a59a317e681f850761d8860b6b5c8d63c96
Vincent Liussyaputra <liusvinv@gmail.com>
2023-04-30 03:41:19 +0700

    Add card selection

--- src/CardManager.jsx
+++ src/CardManager.jsx
@@ -28,7 +28,8 @@ const StyledCards = styled.div`
 const SLOT_STATE = {
   CLOSED:0,
   OPEN:1,
-  SOLVED:2
+  SOLVED:2,
+  SELECTED:3
 }
 
 function CardManager() {
@@ -38,7 +39,7 @@ function CardManager() {
       id:24,
       slotState:SLOT_STATE.SOLVED
     },{
-      id:24,
+      id:15,
       slotState:0
     },
     {
@@ -46,23 +47,23 @@ function CardManager() {
       slotState:0
     }
     ,{
-      id:24,
+      id:44,
       slotState:0
     }
     ,{
-      id:24,
+      id:13,
       slotState:0
     }
     ,{
-      id:24,
+      id:15,
       slotState:0
     }
     ,{
-      id:24,
+      id:13,
       slotState:0
     }
     ,{
-      id:24,
+      id:12,
       slotState:0
     }
     ,{
@@ -73,23 +74,78 @@ function CardManager() {
   ]
   const [slots,setSlots] = useState(defaultSlot);
 
-  function renderCards(){
+  const [selectedSlot,setSelectedSlot] = useState([]);
+  
+
+  function addToSelectedSlot(id,card){
+    // If Two Cards already selected then don't add anymore card
+    if(selectedSlot.length >= 2) {
+      return false;
+    }
+
+    setSelectedSlot(prev =>  {
+      const updated = [...prev];
+      updated.push({...card, index:id});
+      return updated;
+    })
+    return true;
+
+    
+  }
+
+  useEffect(()=>{
+    if(selectedSlot.length >= 2){
+      compareCards();
+    }
+  },[selectedSlot])
+
+
+  function compareCards(){
+    console.log('comparing Card');
+    if(selectedSlot[0].id === selectedSlot[1].id){
+      // Card is the same
+      console.log('card is the same');
+      selectedSlot.forEach((slot)=>{
+        setCardState(slot.index,SLOT_STATE.SOLVED);
+      })
+    }else{
+      // Card is not 
+      // Flip both again
+      selectedSlot.forEach((slot)=>{
+          setCardState(slot.index,SLOT_STATE.CLOSED);
+      })
+    }
+    // Clear Selected
+    setSelectedSlot([]);
+  }
+  function renderCards(){ 
     return slots.map((slot,index)=> {
       const handleReveal = ()=>{
         if(slot.slotState === SLOT_STATE.SOLVED){
           return;
         }
-
-        setSlots(prev => {
-          const updatedSlot = prev.map(prev => prev);
-          updatedSlot[index] = {...prev[index],slotState:prev[index].slotState === SLOT_STATE.OPEN ? SLOT_STATE.CLOSED : SLOT_STATE.OPEN};
-          return updatedSlot;
-        })
+        if(slot.slotState === SLOT_STATE.CLOSED){
+          if(addToSelectedSlot(index,slot)){
+            setCardState(index,SLOT_STATE.SELECTED);
+            return;
+          }
+        }
+        console.log(index);
+        setCardState(index,slot.slotState === SLOT_STATE.OPEN ? SLOT_STATE.CLOSED : SLOT_STATE.OPEN);
       }
       const isSolved = slot.slotState === SLOT_STATE.SOLVED;
-      return <Card onReveal={handleReveal} isVisible={slot.slotState === 1} blockReveal={isSolved}  isSolved={isSolved} cardId={slot.id}></Card>
+      return <Card onReveal={handleReveal} slotState={slot.slotState} isVisible={slot.slotState === 1} blockReveal={slot.slotState === SLOT_STATE.SELECTED || isSolved || selectedSlot && selectedSlot.length >= 2}  isSolved={isSolved} cardId={slot.id}></Card>
     })
   }
+
+  function setCardState(id,toSet){
+    setSlots(prev => {
+      const updatedSlot = prev.map(prevMap => prevMap);
+      updatedSlot[id] = {...prev[id],slotState:toSet};
+      return updatedSlot;
+    })
+  }
+
   return (
     <>
       <StyledCards>
@@ -97,17 +153,24 @@ function CardManager() {
       </StyledCards>
 
       <p>{JSON.stringify(slots)}</p>
+      <h2>Selected Slot</h2>
+      <p>{JSON.stringify(selectedSlot)}</p>
     </>
   )
 }
 
-function Card({isVisible, onReveal,blockReveal,cardId,isSolved}){
+function Card({isVisible, onReveal,blockReveal,cardId,isSolved,slotState}){
 
   const [visible,setVisible] = useState(isVisible || false);
+  const [_slotState,setSlotState] = useState(slotState|| 0);
 
   useEffect(()=>{
+    console.log(isVisible);
     setVisible(isVisible);
   },[isVisible])
+  useEffect(()=>{
+    setSlotState(_slotState);
+  },[slotState])
   
   const renderCard = [
     <div className='card_closed'>
