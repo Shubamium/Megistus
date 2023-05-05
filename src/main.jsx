24910a1917a2748e6557b5a505de031bd5b56d28
Vincent Liussyaputra <liusvinv@gmail.com>
2023-05-05 23:52:01 +0700

    Add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Game from './Game.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+const route = createBrowserRouter([{
+  path:'/',
+  element:<Home/>
+}]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={route}>
+    </RouterProvider>
   </React.StrictMode>,
 )
